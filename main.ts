6efeab0d127453937761d0d8edb0994fd2e27f54
prince <26970055+wkk778@users.noreply.github.com>
2025-12-16 11:22:42 +0800

    1

---
+++ main.ts
@@ -0,0 +1,12 @@
+import { serveDir } from "https://deno.land/std@0.224.0/http/file_server.ts";
+
+Deno.serve((req) => {
+  const url = new URL(req.url);
+
+  return serveDir(req, {
+    fsRoot: "dist",
+    urlRoot: "",
+    showDirListing: false,
+    enableCors: true,
+  });
+});
